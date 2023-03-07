4ec9c09829351dd63214dd5a19fc7f896c4b2d2c
eephi <nana7mi@protonmail.com>
2023-03-06 19:56:03 -0600

    Site updated: 2023-03-06 19:56:01

--- terminal.js
+++ terminal.js
@@ -3,34 +3,74 @@ function handleKeyPress(event) {
         execute();
     }
 }
+
 function execute() {
-    const input = document.getElementById("command").value;
-    const terminal = document.getElementById("output");
+    const input = document.getElementById("input").value;
+    const output = document.getElementById("output");
     const command = input.toLowerCase().trim();
-    let output = "";
+    let reply = "";
 
     switch (command) {
         case "":
             break;
+        case "welcome":
+            reply = "おかえりなさいませ、ご主人様！"
+            break;
         case "help":
-            output = "The user guide has not been finished yet.";
+            reply = "The user guide has not been finished yet.";
+            break;
+        case "ls":
+            reply = "Total 3. Click on &#x60;&lt;NAME&gt;&#x60; to open a folder.";
+            table = createTable();
+            break;
+        case "clear":
             break;
         case "about":
-            output = "来自1885的互联网偶像！王牌级gamer！" + "<br>" + "关注永雏塔菲谢谢喵！";
+            reply = "来自1885的互联网偶像！王牌级gamer！" + "<br>" + "关注永雏塔菲谢谢喵！";
             break;
         default:
-            output = input + ": command not found";
+            reply = input + ": command not found";
             break;
     }
 
     const p = document.createElement("p");
-    p.innerHTML = "<br>" + output;
-    p.insertAdjacentHTML("afterbegin", `<span class="output-prefix">:~$ ` + input + "</span>");
-    p.insertAdjacentHTML("afterbegin", `<span class="output-username">you@eephibot</span>`);
-    terminal.appendChild(p);
-    terminal.scrollTop = terminal.scrollHeight;
-    document.getElementById("command").value = "";
+
+    if (command == "ls") {
+        const t = document.createElement("table-container");
+        t.appendChild(table);
+        p.appendChild(table)
+    } else {
+        p.insertAdjacentHTML("beforeend", "<br>");
+    }
+
+    p.insertAdjacentHTML("beforeend", reply);
+    p.insertAdjacentHTML("afterbegin", `<span class="command">:~$ ` + input + "</span>");
+    p.insertAdjacentHTML("afterbegin", `<span class="username">you@eephibot</span>`);
+
+    output.appendChild(p);    
+    if (command == "clear") {
+        clearTerminal();
+    }
+    output.scrollTop = output.scrollHeight;
+    document.getElementById("input").value = "";
 }
 
+function generatePrefix() {
+    const elements = document.getElementsByClassName("prefix");
+
+    for (let i = 0; i < elements.length; i++) {
+        const content = document.createElement("span");
+        content.innerHTML = "<span class=username>you@eephibot</span><span class=command>:~&#36;&nbsp;</span>";
+        elements[i].appendChild(content);
+    }
+}
+
+function clearTerminal() {
+    document.getElementById("output").innerHTML = "" ;
+}
+
+// generate prefix when the page is loaded
+window.addEventListener("load", generatePrefix);
+
 // focus on the input field when the page loads
-document.getElementById("command").focus();
+document.getElementById("input").focus();
