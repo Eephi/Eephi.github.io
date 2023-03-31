7bfd616b32c5dad5ecddcf3a80009087c9069b2d
eephi <nana7mi@protonmail.com>
2023-03-30 22:18:00 -0500

    Site updated: 2023-03-30 22:17:38

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ const app = Vue.createApp({
             showMenu: false,
             barLocal: 0,
             renderers: [],
+            showSearch: false,
+            searchData: false,
+            searchQuery: "",
+            datas: []
         };
     },
     created() {
@@ -29,6 +33,115 @@ const app = Vue.createApp({
             let background = this.$refs.homeBackground;
             window.scrollTo({ top: background.offsetHeight, behavior: "smooth" });
         },
+        searchClick(){
+            this.showSearch = !this.showSearch;
+            if (!this.searchData) {
+                this.searchFunc(window.searchPath);
+            }
+            this.searchData = true;
+        },
+        closeClick() {
+            this.showSearch = false;
+        },
+        searchFunc(path) {
+            'use strict';
+            $.ajax({
+                url: path,
+                dataType: "xml",
+                success: (xmlResponse) => {
+                    // get the contents from search data
+                    this.datas = $( "entry", xmlResponse ).map(function() {
+                        return {
+                            title: $( "title", this ).text(),
+                            content: $("content",this).text(),
+                            url: $( "url" , this).text()
+                        };
+                    }).get();
+                },
+            });
+        },
+        handleSearch() {
+            let content_id = "local-search-result";
+            let resultContent = document.getElementById(content_id);
+            let str='<ul class=\"search-result-list\">';                
+            let keywords = this.searchQuery.trim().toLowerCase().split(/[\s\-]+/);
+
+            resultContent.innerHTML = "";
+            if (this.searchQuery.trim().length <= 0) {
+                return;
+            }
+            // perform local searching
+            this.datas.forEach(function(data) {
+                let isMatch = true;
+                let content_index = [];
+                let data_title = data.title.trim().toLowerCase();
+                let data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
+                let data_url = data.url;
+                let index_title, index_content, first_occur = -1;
+                // only match artiles with not empty titles and contents
+                if(data_title != '' && data_content != '') {
+                    keywords.forEach(function(keyword, i) {
+                        index_title = data_title.indexOf(keyword);
+                        index_content = data_content.indexOf(keyword);
+                        if( index_title < 0 && index_content < 0 ){
+                            isMatch = false;
+                        } else {
+                            if (index_content < 0) {
+                                index_content = 0;
+                            }
+                            if (i == 0) {
+                                first_occur = index_content;
+                            }
+                        }
+                    });
+                }
+                // show search results
+                if (isMatch) {
+                    var match_title = data.title;
+                    keywords.forEach(function(keyword) {
+                        var regS = new RegExp(keyword, "gi");
+                        match_title = match_title.replace(regS, function(match) {
+                            return "<span class=\"search-keyword\">"+match+"</span>";
+                        })
+                    });
+                    str += "<div class='local-search__hit-item'><a href='" + data_url + "'><span class='search-result-title'>"+ match_title +"</span>";
+
+                    var content = data.content.trim().replace(/<[^>]+>/g,"");
+                    if (first_occur >= 0) {
+                        // cut out 100 characters
+                        var start = first_occur - 20;
+                        var end = first_occur + 80;
+
+                        if(start < 0){
+                            start = 0;
+                        }
+                        if(start == 0){
+                            end = 100;
+                        }
+                        if(end > content.length){
+                            end = content.length;
+                        }
+
+                        var match_content = content.substr(start, end); 
+
+                        // highlight all keywords
+                        keywords.forEach(function(keyword){
+                            var regS = new RegExp(keyword, "gi");
+                            match_content = match_content.replace(regS, function(match) {
+                                return "<span class=\"search-keyword\">"+match+"</span>";
+                            })
+                        });
+                        str += "<p class=\"search-result\">" + match_content +"...</p>";
+                    }
+                    let split = data_url.split("/")
+                    let date = split[1] + "-" + split[2] + "-" + split[3];
+                    str += "<span class='search-result-date'>" + date + "</span>";
+                    str += "</a></div>";
+                }
+            });
+            str += "</ul>";
+            resultContent.innerHTML = str;
+        },
         render() {
             if (typeof this.renderers === "undefined") return;
             for (let i of this.renderers) i();
@@ -40,6 +153,7 @@ const app = Vue.createApp({
             let newlocal = document.documentElement.scrollTop;
             if (newlocal <= background.offsetHeight - 100) {
                 menu.classList.add("menu-color");
+                menu.innerHTML(newlocal)
             }
             else {
                 menu.classList.remove("menu-color");
